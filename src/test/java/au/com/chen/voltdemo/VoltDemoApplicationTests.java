be3e70b92a439f02797b715f76de435ca728dcd1
Chen Liu <liuchenhts@gmail.com>
2020-01-07 22:07:55 +1100

    init commit

---
+++ src/test/java/au/com/chen/voltdemo/VoltDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.chen.voltdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
